c2d1b12ff386fb5c48ba139a50429d611aa91090
Szanda Mate <szandamate88@gmail.com>
2020-03-08 19:55:43 +0100

    Move to Typesript

--- diamond.ts
+++ diamond.ts
@@ -1,6 +1,8 @@
-/* let koz = ''
+export{};
+
+let koz = ''
 let csil = '*'
-let lineCount = 4 */
+let lineCount = 4
 
 
 for (let i = 1; i <= lineCount; i++) {
@@ -23,17 +25,14 @@ console.log('line count :',lineCount ) */
 //csill=9
 //lineCount=4
 
-let csil2 = '*'
+let csil2 = ''
 for (let p = lineCount - 1; p >= 0; p--) {
 	for (let b = lineCount - 1; b >= 0; b--) {
-		if (b % 2 !== 0) {
-			/* csil += "*" */
-			csil.pop()
-			//csil=['*','*','*','*','*','*','*','*','*']
-		}
+		
 	}
-	console.log(koz + csil)
 	koz+= ' '
+	console.log(koz + csil)
+	
 }
 
 console.log(koz.length, csil.length)
