637acf507280e6ede888f1edf3cf1360f61630b0
Szanda Mate <szandamate88@gmail.com>
2020-03-08 20:42:11 +0100

    Diamond done

--- diamond.ts
+++ diamond.ts
@@ -4,37 +4,28 @@ let koz = ''
 let csil = '*'
 let lineCount = 4
 
-
 for (let i = 1; i <= lineCount; i++) {
 	for (let j = 1; lineCount - i >= j; j++) {
 		koz += ' '
-
-
-
-
 	}
 	console.log(koz + csil)
+
 	koz = ''
 	csil += '**'
 }
 
-/* console.log(koz.length, csil.length)
+let csil2 =''
+let starLength = csil.length-4
 
-console.log('line count :',lineCount ) */
-//köz=0
-//csill=9
-//lineCount=4
-
-let csil2 = ''
-for (let p = lineCount - 1; p >= 0; p--) {
-	for (let b = lineCount - 1; b >= 0; b--) {
-		
+for (let p = lineCount-1; p > 0; p--) {
+	for (let b= starLength;b > 0; b--) {
+		csil2+='*'
 	}
 	koz+= ' '
-	console.log(koz + csil)
 	
+	console.log(koz + csil2)
+
+	csil2=''
+	starLength-=2
 }
 
-console.log(koz.length, csil.length)
-
-console.log('line count :', lineCount)
