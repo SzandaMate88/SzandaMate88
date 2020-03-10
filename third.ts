bdf5ddd61d444aee91113ce21bb1e171c3009671
Szanda Mate <szandamate88@gmail.com>
2020-03-10 13:02:53 +0100

    Ex Third Done

---
+++ third.ts
@@ -0,0 +1,8 @@
+export{};
+
+// -  Create a variable named `numbers`
+//    with the following content: `[1, 3, 5, 7]`
+// -  Print the third element of `numbers`
+
+let numbers = [1,3,5,7];
+console.log(numbers[2]);
