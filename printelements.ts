ca425f4470aa6f0857e9de254d9b05b499c989ca
Szanda Mate <szandamate88@gmail.com>
2020-03-10 14:18:52 +0100

    Ex printElements Done

---
+++ printelements.ts
@@ -0,0 +1,14 @@
+export{};
+
+// -  Create a variable named `numbers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `numbers` to the console*
+// *hint: use a loop, console.log(numbers) won't cut it
+// -  bonus for using the correct built in array method
+
+let numbers = [];
+numbers.push(4, 5, 6, 7);
+
+numbers.forEach(function(item, index) {
+console.log(item,)
+})
+
