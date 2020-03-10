8f302059de8170f57688d00df39ce0a784fa2026
Szanda Mate <szandamate88@gmail.com>
2020-03-10 13:35:23 +0100

    Ex Sumelements Done

---
+++ sumelement.ts
@@ -0,0 +1,13 @@
+export{};
+// - Create a variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+let numbers = [];
+numbers.push(54 ,23 ,66 ,12 )
+
+
+
+
+
+console.log(numbers[1]+numbers[2])
