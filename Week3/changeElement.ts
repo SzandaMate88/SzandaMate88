4ddbf302fd939a13edeb197db8fb57211e76a643
Szanda Mate <szandamate88@gmail.com>
2020-03-12 20:26:27 +0100

    EX ChangeElement Fianly Done

--- Week3/changeElement.ts
+++ Week3/changeElement.ts
@@ -4,52 +4,15 @@ export{};
 // -  Change the 8 to 4 with the `.map` method 
 // -  Print the fourth element as a test
 
-/* let numbers = [];
-numbers.push(1, 2, 3, 8, 5, 6);
-let sortedNumbers: number[] = numbers.sort(compare);
-
-function compare(a: number, b: number):number{
-  return a-b
-}
-console.log(sortedNumbers);
- */
+let sortedNumbers: number[] = [1, 2, 3, 8, 5, 6]
 
 
 
-let numbers  = [];
-numbers.push(1, 2, 3, 8, 5, 6);
-
-numbers = numbers.filter(function(value){
-if(value = 3){
-  numbers.push[3](4)
-}
-
-
-})
-
-
-
-console.log(numbers)
-
-
-
-/* let sortedNumbers: number[] = numbers.sort(function(a:number, b:number){;
-  return a-b
-})
-function min(a: number, b: number): number {
-  for (let i: number = 0; i < 10; i++){
-    if (i === 8){
-      a = a-b
-      return a
-    }
+let finalNum: number[]=sortedNumbers.map((value: number, index: number) =>{
+  if(value === 8){
+    return 4
+  }else{
+    return value
   }
-}
-
-
-function compare(a: number, b: number):number{
-  return a-b
-}
- console.log(sortedNumbers);
- */
-
-
+})
+console.log(finalNum)
