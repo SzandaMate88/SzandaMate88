a58f17fe4ed2518b2a072ead3c8472fd1011c5f3
Szanda Mate <szandamate88@gmail.com>
2020-03-13 00:47:50 +0100

    Refresh

---
+++ Week3/colors.ts
@@ -0,0 +1,22 @@
+export{};
+
+// -  Create a two dimensional list
+//    which can contain the different shades of specified colors
+// -  In `colors[0]` store the shades of green:
+//    `"lime", "forest green", "olive", "pale green", "spring green"`
+// -  In `colors[1]` store the shades of red:
+//    `"orange red", "red", "tomato"`
+// -  In `colors[2]` store the shades of pink:
+//    `"orchid", "violet", "pink", "hot pink"`
+
+let colors: string [][] =[
+  ['green','lime', 'forest green', 'olive', 'pale green'],
+  ['red', 'orange red', 'red', 'tomato'],
+  ['pink', 'orchid', 'violet', 'pink', 'hot pink']
+];
+console.log (colors)
+console.log(colors[1][3]);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
--- Week3/incrementElement.ts
+++ Week3/incrementElement.ts
@@ -7,12 +7,12 @@ numbers.push(1, 2, 3, 4, 5);
 
 
 numbers = numbers.map((value:number, index: number, array:[]) => {
-  if(index === 2) {
-    let i: number = value+1
+  if (index === 2) {
+    let i: number = value+1;
     return i;
-  }else{
+  }else {
     return value;
   }
-}) 
-
+}); 
 console.log(numbers);
+console.log(numbers[3]);
