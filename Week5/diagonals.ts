fe615bfb5a7e23b5b605c09c03b193c2d5ad0ec1
Szanda Mate <szandamate88@gmail.com>
2020-03-31 20:41:52 +0200

    EX diagonals Done

---
+++ Week5/diagonals.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw the canvas' diagonals.
+// If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+ctx.beginPath();
+ctx.strokeStyle = 'green';
+ctx.moveTo(0,0)
+ctx.lineTo(600,400);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.moveTo(600,0)
+ctx.lineTo(0,400);
+ctx.stroke();
+ctx.closePath();
