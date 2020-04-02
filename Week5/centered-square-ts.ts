68c931a9eba766f926de26b80c73685d9eb60373
Szanda Mate <szandamate88@gmail.com>
2020-04-02 16:57:05 +0200

    Ex centered Square Done

---
+++ Week5/centered-square-ts.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw a green 10x10 square to the center of the canvas.
+
+ctx.fillRect(295,195,10,10);
+ctx.fillStyle = 'green';
--- Week5/horizontal-lines.ts
+++ Week5/horizontal-lines.ts
@@ -10,30 +10,17 @@ const ctx = canvas.getContext('2d');
 // and draws a 50 long horizontal line from that point.
 // Draw at least 3 lines with that function using a loop.
 
-/* function draw (x: number, y: number) {
-  for ( let i: number = 0; i < 3; i++) {
-    
-    ctx.beginPath();
-    ctx.strokeStyle = 'red';
-    ctx.moveTo(x, y);
-    ctx.lineTo(x+50, y);
-    ctx.stroke();
-    x = x+50;
-    ctx.closePath();
-  }
-}draw (200,100) */
+
 
 function draw (x: number, y: number) {
   for (let i: number = 0; i<3; i++){
     x = x+50;
-    y = y+50;
-  
-ctx.beginPath();
-ctx.strokeStyle = 'red';
-ctx.moveTo(x,y);
-ctx.lineTo(x+50, y);
-ctx.stroke();
-ctx.closePath;
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.moveTo(x,y);
+    ctx.lineTo(x+50, y);
+    ctx.stroke();
+    ctx.closePath;
 }
 }
 draw (150,200);
