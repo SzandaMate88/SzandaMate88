67f66306db790dbe411bc113a67221a750f10969
Szanda Mate <szandamate88@gmail.com>
2020-04-02 21:26:45 +0200

    Ex triangles Done

---
+++ Week5/triangles.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+const baseTriangleCount: number = 15;
+const triangleWidth: number = 400/baseTriangleCount;
+const triangleHeightSquare: number = (triangleWidth*triangleWidth) - ((triangleWidth/2)*(triangleWidth/2));
+const triangleHeight: number =canvas.height/ Math.sqrt(triangleHeightSquare)
+const halfWidth: number = triangleWidth/2
+
+ctx.beginPath();
+ctx.strokeStyle = 'blue';
+
+//horizontális
+
+for (let i: number = 0; i < baseTriangleCount; i++) {
+  ctx.moveTo(100 + i * halfWidth,400 - i * (canvas.height/baseTriangleCount));
+  ctx.lineTo(500 - i * halfWidth,400 - i *(canvas.height/ baseTriangleCount));
+  ctx.stroke();
+}
+
+// '\'
+
+for (let i: number = 0; i < baseTriangleCount; i++) {
+  ctx.moveTo(300 - i * halfWidth, 0 + i * (canvas.height/baseTriangleCount));
+  ctx.lineTo(500 - i * triangleWidth, 400 );
+  ctx.stroke();
+
+}
+
+// '/'
+
+for (let i: number = 0; i < baseTriangleCount; i++) {
+  ctx.moveTo(100+ i * triangleWidth, 400);
+  ctx.lineTo(300 + i * halfWidth , 0 + i *(canvas.height/baseTriangleCount) );
+  ctx.stroke();
+}
