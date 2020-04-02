bdd276650111449a91d502a50305463def87b471
Szanda Mate <szandamate88@gmail.com>
2020-04-02 16:51:28 +0200

    Ex Horizontal-Line Done

--- Week5/horizontal-lines.ts
+++ Week5/horizontal-lines.ts
@@ -6,9 +6,22 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // Create a function that draws a single line and takes 2 parameters:
-// The x and y coordinates of the line's starting point
-// and draws a line from that point to the center of the canvas
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
 // Draw at least 3 lines with that function using a loop.
+
+/* function draw (x: number, y: number) {
+  for ( let i: number = 0; i < 3; i++) {
+    
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.moveTo(x, y);
+    ctx.lineTo(x+50, y);
+    ctx.stroke();
+    x = x+50;
+    ctx.closePath();
+  }
+}draw (200,100) */
 
 function draw (x: number, y: number) {
   for (let i: number = 0; i<3; i++){
@@ -18,9 +31,10 @@ function draw (x: number, y: number) {
 ctx.beginPath();
 ctx.strokeStyle = 'red';
 ctx.moveTo(x,y);
-ctx.lineTo(canvas.width/2 ,canvas.height/2);
+ctx.lineTo(x+50, y);
 ctx.stroke();
 ctx.closePath;
 }
 }
 draw (150,200);
+
