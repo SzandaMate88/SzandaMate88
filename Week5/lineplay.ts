0f4a195cd4a4b689629ee4184f705333e6bcc425
Szanda Mate <szandamate88@gmail.com>
2020-04-03 10:30:18 +0200

    EX linePlay Done

---
+++ Week5/lineplay.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+const lineCount: number = 15;
+const border: number = 20;
+const LineDistanceX = (canvas.width-border)/lineCount;
+const LineDistanceY: number = (canvas.height-border)/lineCount;
+
+for ( let i: number = 0; i < lineCount -1; i++) {
+  ctx.beginPath();
+  ctx.strokeStyle = 'purple';
+  ctx.moveTo((0+border+LineDistanceX)+ i * LineDistanceX, 0+border);
+  ctx.lineTo(600-border,(0+border) + i * LineDistanceY);
+  ctx.stroke();
+  ctx.closePath();
+}
+
+for (let j: number = 0; j < lineCount -1; j++) {
+  ctx.beginPath();
+  ctx.strokeStyle = 'green';
+  ctx.moveTo((0+border),(0+border) + j * LineDistanceY);
+  ctx.lineTo((0+border + LineDistanceX) + j * LineDistanceX,400-border);
+  ctx.stroke();
+}
